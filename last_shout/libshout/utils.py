599a0ed59ecec1ce0255781dbad131cd2a916567
Brian Pepple <bdpepple@gmail.com>
2019-12-22 09:51:56 -0500

    Add some functions to build twitter string

---
+++ last_shout/libshout/utils.py
@@ -0,0 +1,41 @@
+""" Various utilites """
+import datetime
+
+MUSICAL_NOTE = u"\u266A"
+
+
+def periods_to_string(period):
+    """ Function to convert time period setting to string value """
+    now = datetime.datetime.now()
+    switcher = {
+        "overall": " All-Time ",
+        "7day": " Weekly ",
+        "1month": " Monthly ",
+        "3month": " Quarterly ",
+        "6month": " Semi-Annual ",
+        "12month": " " + str(now.year) + " ",
+    }
+
+    return switcher.get(period, " ")
+
+
+def build_twitter_string(artists, period):
+    """ Function to convert Last.fm result to a string that will be posted to Twitter """
+    total = len(artists)
+    txt = (
+        MUSICAL_NOTE
+        + " My"
+        + periods_to_string(period)
+        + "Top "
+        + str(total)
+        + " #lastfm artists: "
+    )
+
+    for count, artist in enumerate(artists, 1):
+        txt += artist.item.name + " (" + artist.weight + ")"
+        if count < total - 1:
+            txt += ", "
+        elif count == total - 1:
+            txt += " & "
+
+    return txt
