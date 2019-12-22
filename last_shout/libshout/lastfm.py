174f31317f45bc24ec0fbe37e1e437ccf42d936d
Brian Pepple <bdpepple@gmail.com>
2019-12-22 09:50:58 -0500

    Add function to retrieve last.fm statistics

---
+++ last_shout/libshout/lastfm.py
@@ -0,0 +1,11 @@
+""" Function to users top artist from Last.fm """
+import pylast
+
+
+def get_top_artist(last_access_key, username, number, period):
+    """ Simple function returning the users top artists """
+    network = pylast.LastFMNetwork(last_access_key)
+    user = network.get_user(username)
+    artists = user.get_top_artists(period=period, limit=number)
+
+    return artists
