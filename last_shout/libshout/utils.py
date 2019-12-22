bd696f05234f85d9ea1ac955f97ce7579831defc
Brian Pepple <bdpepple@gmail.com>
2019-12-22 10:34:21 -0500

    Make note to add truncate tweet function

--- last_shout/libshout/utils.py
+++ last_shout/libshout/utils.py
@@ -19,6 +19,9 @@ def periods_to_string(period):
     return switcher.get(period, " ")
 
 
+# TODO: Add function to truncate tweet if it's length is greater than 280 characters.
+
+
 def build_twitter_string(artists, period):
     """ Function to convert Last.fm result to a string that will be posted to Twitter """
     total = len(artists)
