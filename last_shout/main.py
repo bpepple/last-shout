f92513a6972d1f2718949ba8f007f3e46b1b0830
Brian Pepple <bdpepple@gmail.com>
2020-01-11 09:32:33 -0500

    Make sending a tweet optional

--- last_shout/main.py
+++ last_shout/main.py
@@ -9,6 +9,25 @@ from .libshout.twitter import send_tweet
 from .libshout.utils import build_twitter_string
 
 
+def has_lastfm_credentials(settings):
+    if not settings.last_user and settings.last_access_key:
+        return False
+
+    return True
+
+
+def has_twitter_credentials(settings):
+    if not (
+        settings.consumer_key
+        and settings.consumer_key
+        and settings.access_key
+        and settings.access_secret
+    ):
+        return False
+
+    return True
+
+
 def main():
     """ Main Function """
     settings = LastShoutSettings()
@@ -37,7 +56,7 @@ def main():
 
     # Save Last.fm options
     if opts.set_lastfm:
-        if settings.last_user and settings.last_access_key:
+        if has_lastfm_credentials(settings):
             settings.save()
         else:
             print("Missing Last.fm credetials. Unable to save.")
@@ -45,26 +64,14 @@ def main():
 
     # Save Twitter options
     if opts.set_twitter:
-        if (
-            settings.consumer_key
-            and settings.consumer_secret
-            and settings.access_key
-            and settings.access_secret
-        ):
+        if has_twitter_credentials(settings):
             settings.save()
         else:
             print("Missing Twitter credentials. Unable to save.")
             sys.exit(0)
 
     # If Last.fm or Twitter credentials are missing exit
-    if (
-        not settings.last_user
-        or not settings.last_access_key
-        or not settings.consumer_key
-        or not settings.consumer_secret
-        or not settings.access_key
-        or not settings.access_secret
-    ):
+    if not has_lastfm_credentials(settings) or not has_twitter_credentials(settings):
         print("Missing Last.fm or Twitter credentials. Exitting...")
         sys.exit(2)
 
@@ -72,7 +79,12 @@ def main():
         settings.last_access_key, settings.last_user, opts.number, opts.period
     )
     twitter_text = build_twitter_string(artists, opts.period)
-    send_tweet(settings, twitter_text, None)
+
+    if opts.tweet:
+        status = send_tweet(settings, twitter_text, None)
+        print(f"Last.fm statistics posted to Twitter at {status.created_at}")
+    else:
+        print(twitter_text)
 
 
 if __name__ == "__main__":
